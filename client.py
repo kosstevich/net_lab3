7dae0c66cfaa04277e74518d04ea3f7d6e127a1e
Kostevich Arseniy <kostevich_a@bk.ru>
2023-12-13 00:31:58 +0300

    version: 0.0.1

--- client.py
+++ client.py
@@ -6,20 +6,31 @@ def udp_client(ip:str = "127.0.0.1", port:int = 5000):
         while True:
             data = input('Enter message: ')
             udp_socket.sendto(str.encode(data), (ip, port))
-            input_data = udp_socket.recvfrom(1024)
+            input_data, addr = udp_socket.recvfrom(1024)
+            print(input_data.decode("utf-8"))
 
-    except Exception as e:
+    except:
         udp_socket.close()
         print("Error! Server stopped!")
-        print("Error log:")
-        print(e)
 
 def tcp_client(ip:str = "127.0.0.1", port:int = 5000):
-    pass
-
+    tcp_socket = socket(AF_INET, SOCK_STREAM)
+    tcp_socket.connect((ip,port))
+    try:
+        while True:
+            data = input('Enter message: ')
+            tcp_socket.send(data.encode("utf-8"))
+            input_data = tcp_socket.recv(1024)
+            print(input_data.decode("utf-8"))
+    except:
+        tcp_socket.close()
+        print("Error! Server stopped!")
+    
 if __name__ == "__main__":
     try:
-        udp_client("10.1.6.120")
-    except Exception as e:
-        udp_socket.close()
+        #udp_client("10.1.6.120")
+        #tcp_client()
+        #udp_client()
+        tcp_client("10.1.6.120")
+    except:
         print("Error! Server stopped!")
--- server.py
+++ server.py
@@ -2,38 +2,50 @@ from socket import *
 
 def tcp_server(ip:str = "127.0.0.1", port:int = 5000):
     tcp_socket = socket(AF_INET, SOCK_STREAM)
+    tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     tcp_socket.bind((ip,port))
+    tcp_socket.listen(0)
     print("Server running")
-    try:  
-        pass
-    except:
+    try:
+        while True:
+            connection, addr = tcp_socket.accept()
+            while True:
+                data = connection.recv(1024).decode("utf-8")
+                print('Client addr: ', addr)
+                print('Client data: ', data)
+                output_data = "Response: " + data
+                connection.send(output_data.encode("utf-8"))
+    except Exception as e:
+        connection.close()
         tcp_socket.close()
         print("Error! Server stopped!")
-        print("Error log:")
-        print(e)
 
 
 def udp_server(ip:str = "127.0.0.1", port:int = 5000):
     udp_socket = socket(AF_INET, SOCK_DGRAM)
+    udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     udp_socket.bind((ip,port))
     print("Server running")
     try:  
         while True:
             data, addr = udp_socket.recvfrom(1024)
             print('Client addr: ', addr)
-            print('Client data: ', data)
-            udp_socket.sendto(data, addr)
-    except Exception as e:
+            print('Client data: ', data.decode("utf-8"))
+            output_data = "Response: " + data.decode("utf-8")
+            udp_socket.sendto(output_data.encode("utf-8"), addr)
+
+    except:
         udp_socket.close()
         print("Error! Server stopped!")
-        print("Error log:")
-        print(e)
 
 if __name__ == "__main__":
     try:
-        udp_server("10.1.6.120")
+        tcp_server("10.1.6.120")
+        #tcp_server()
+        #udp_server()
+        #udp_server("10.1.6.120")
     except Exception as e:
-        udp_socket.close()
         print("Error! Server stopped!")
+        print(e)
     
 
